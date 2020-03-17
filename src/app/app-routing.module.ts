096bf31665544d320f94f5b013768bfcf88050fb
satishderle123 <55485823+satishderle123@users.noreply.github.com>
2020-03-17 15:01:40 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,37 @@
+//---- why my routing file not updating 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MemberViewComponent } from './member-view/member-view.component';
+import { MemberReportsComponent } from './member-reports/member-reports.component';
+import { MemberGridComponent } from './member-grid/member-grid.component';
+import { MemberAboutComponent} from './member-about/member-about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'member-view',
+    component: MemberViewComponent,
+    data: { title: 'Member Master' }
+  },
+  {
+    path: 'member-reports',
+    component: MemberReportsComponent,
+    data: { title: 'Reports' }
+  },
+  {
+    path: 'member-grid',
+    component: MemberGridComponent,
+    data: { title: 'Grids' }
+  },
+  {
+    path: 'member-about',
+    component: MemberAboutComponent,
+    data: { title: 'About Info.' }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-//I have added this line in Github.
