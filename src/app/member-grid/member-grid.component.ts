c639f5087bfff08b7fc361b77bea1210ab5d119e
satishderle123 <55485823+satishderle123@users.noreply.github.com>
2020-03-17 11:43:24 +0530

    on 17 mar 2020

---
+++ src/app/member-grid/member-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-grid',
+  templateUrl: './member-grid.component.html',
+  styleUrls: ['./member-grid.component.css']
+})
+export class MemberGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/member-reports/member-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-reports',
+  templateUrl: './member-reports.component.html',
+  styleUrls: ['./member-reports.component.css']
+})
+export class MemberReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
