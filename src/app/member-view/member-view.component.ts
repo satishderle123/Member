7cc66c0758c28765ed3842fcac4b29bd353c8a4d
satishderle123 <55485823+satishderle123@users.noreply.github.com>
2020-03-18 17:39:52 +0530

    enf of 18 mar

--- src/app/member-view/member-view.component.ts
+++ src/app/member-view/member-view.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-member-view',
   templateUrl: './member-view.component.html',
   styleUrls: ['./member-view.component.css']
 })
-export class MemberViewComponent implements OnInit {
+export class MemberViewComponent  {
+  memberViewForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  
   ngOnInit() {
-  }
-
+    this.memberViewForm = this.fb.group({
+        MemberName: ['', [Validators.required, Validators.minLength(6)],Validators.name],
+        MemNo: ['', [Validators.required, Validators.minLength(7),Validators.maxLength(7)]],
+        RegNo: ['', [Validators.required]],
+        Addr1: ['', [Validators.required, Validators.minLength(6)]],
+        Addr2: ['', [Validators.required, Validators.minLength(6)]],
+        Village: ['', [Validators.required, Validators.name]],
+    },);
 }
+
+onSubmit() {
+  this.submitted = true;
+  // stop here if form is invalid
+  if (this.memberViewForm.invalid) {
+        return;
+  }
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.memberViewForm.value))
+} //onSubmit
+
+
+} //main closing brace
