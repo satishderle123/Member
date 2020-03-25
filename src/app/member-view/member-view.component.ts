6ce36e459770083d9d57b15f2c6a53bb128c54c9
satishderle123 <55485823+satishderle123@users.noreply.github.com>
2020-03-25 13:17:36 +0530

    on 25 mar at 1.17pm

--- src/app/member-view/member-view.component.ts
+++ src/app/member-view/member-view.component.ts
@@ -1,36 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder,FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-member-view',
   templateUrl: './member-view.component.html',
   styleUrls: ['./member-view.component.css']
 })
-export class MemberViewComponent  {
+export class MemberViewComponent implements OnInit {
   memberViewForm: FormGroup;
   submitted = false;
 
   constructor(private fb: FormBuilder) { }
   
   ngOnInit() {
-    this.memberViewForm = this.fb.group({
-        MemberName: ['', [Validators.required, Validators.minLength(6)],Validators.name],
-        MemNo: ['', [Validators.required, Validators.minLength(7),Validators.maxLength(7)]],
-        RegNo: ['', [Validators.required]],
-        Addr1: ['', [Validators.required, Validators.minLength(6)]],
-        Addr2: ['', [Validators.required, Validators.minLength(6)]],
-        Village: ['', [Validators.required, Validators.name]],
+    /* Initialize any Control here if any like this.cat="SC, ST, OBC, ...." */
+    this.memberViewForm =  this.fb.group({
+      MemNo: ['', [Validators.required, Validators.minLength(7),Validators.maxLength(7)]],
+      RegNo: ['', [Validators.required]],
+      MemberName: ['', [Validators.required, Validators.minLength(6)],Validators.name],
+      Addr1: ['', [Validators.required, Validators.minLength(6)]],
+      Addr2: ['', [Validators.required, Validators.minLength(6)]],
+      Village: ['', [Validators.required, Validators.name]],
+
     },);
 }
 
-onSubmit() {
-  this.submitted = true;
-  // stop here if form is invalid
-  if (this.memberViewForm.invalid) {
-        return;
-  }
-  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.memberViewForm.value))
-} //onSubmit
+  onSubmit() {
+     //this.submitted = true;
+     alert(this.memberViewForm.value);
+     console.log(this.memberViewForm.value);
+     //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.memberViewForm.value))
 
+     //if (this.memberViewForm.invalid) {   /*Stop here if Form is invalid */
+     //   return;
+     // }
+  } //onSubmit
 
 } //main closing brace
